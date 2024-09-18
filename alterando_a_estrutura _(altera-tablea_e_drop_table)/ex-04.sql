4f713af6268b31f43dcd63b1d60e877b1dd8ae66
Mirka Juliet <72041260+MirkaJuliet34@users.noreply.github.com>
2024-09-18 09:41:11 -0300

    ex-04.sql

--- alterando_a_estrutura _(altera-tablea_e_drop_table)/ex-04.sql
+++ alterando_a_estrutura _(altera-tablea_e_drop_table)/ex-04.sql
@@ -23,3 +23,20 @@ alter table cursos
 add column idcurso int first;
 
 describe cursos;
+
+/* Faça o teste */
+
+create table if not exists teste(
+  id int, 
+  nome varchar(10),
+  idade int
+);
+
+insert into teste value 
+('1', 'Pedro', '22'),
+('2', 'Maria', '12'),
+('3', 'Maricota', '77');
+
+select * from teste;
+
+drop table if exists teste;

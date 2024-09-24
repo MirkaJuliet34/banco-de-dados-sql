7df6f72a79e8d6a437732ea528316b4d9a9cdf0d
Mirka Juliet <72041260+MirkaJuliet34@users.noreply.github.com>
2024-09-24 09:25:05 -0300

    wild_card.sql

---
+++ AULA - 18/wild_card.sql
@@ -0,0 +1,26 @@
+SELECT * FROM employees
+WHERE first_name = "s5";
+
+
+SELECT * FROM employees
+WHERE hire_date LIKE "2023%"; 
+
+
+SELECT * FROM employees
+WHERE job LIKE "_ook";
+
+
+SELECT * FROM employees
+WHERE job LIKE "_ook_";
+
+
+SELECT * FROM employees
+WHERE hire_date LIKE "___-01-__";
+
+
+SELECT * FROM employees
+WHERE hire_date LIKE "___-__-02"; 
+
+
+SELECT * FROM employees
+WHERE job LIKE "_a%";

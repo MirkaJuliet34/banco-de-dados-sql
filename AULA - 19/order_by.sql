f51566ad78fdbbfb82ddfdeb664b0854a52b44a3
Mirka Juliet <72041260+MirkaJuliet34@users.noreply.github.com>
2024-09-25 09:43:41 -0300

    order_by.sql

---
+++ AULA - 19/order_by.sql
@@ -0,0 +1,25 @@
+SELECT * FROM employees
+ORDER BY last_name;
+
+SELECT * FROM employees
+ORDER BY last_name DESC;
+
+
+SELECT * FROM employees
+ORDER BY last_name ASC;
+
+
+SELECT * FROM employees
+ORDER BY hire_date DESC;
+
+SELECT * FROM transactions;
+
+SELECT * FROM transactions
+ORDER BY amount;
+
+SELECT * FROM transactions
+ORDER BY amount, customer_id;
+
+
+SELECT * FROM transactions
+ORDER BY amount ASC, customer_id DESC;

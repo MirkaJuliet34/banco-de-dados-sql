5b72a1ecc5af51f8f17b06dab14947c59c1807e3
Mirka Juliet <72041260+MirkaJuliet34@users.noreply.github.com>
2024-09-26 10:16:10 -0300

    limit.sql

---
+++ AULA - 20/limit.sql
@@ -0,0 +1,19 @@
+
+SELECT * FROM customers
+LIMIT 1; 
+
+
+SELECT * FROM customers
+ORDER BY last_name DESC LIMIT 1;
+
+
+SELECT * FROM customers
+LIMIT 1, 1; 
+
+
+SELECT * FROM customers
+LIMIT 10;
+
+
+SELECT * FROM customers
+LIMIT 10, 10; 
